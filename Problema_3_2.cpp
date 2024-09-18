4854414a0ad377fd9b2eae4827e12a4848617835
AndreRevilla <andre.revilla@ucsp.edu.pe>
2024-09-18 07:24:50 -0500

    Update Problema_3_2.cpp

--- Problema_3_2.cpp
+++ Problema_3_2.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool esPrimo(int n){
-    for (int i{2}; i<n ; i++){
-        if (n%i == 0)
+bool esPrimo(int num) {
+    int i=2;
+    if (num<=1){
+        return false;
+    }
+    while(i < num){
+        if (num % i == 0){
             return false;
+        }
+        i++;
     }
     return true;
 }
@@ -18,8 +24,5 @@ void imprimirPrimos(int n){
 }
 
 int main(){
-    int n;
-    cout<<"Digite un entero: ";
-    cin>>n;
-    imprimirPrimos(n);
+    imprimirPrimos(50);
 }
